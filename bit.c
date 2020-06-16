f5993f0c9857d893298ad89baa38e2a6065c6a8d
Stanislav991 <66986825+Stanislav991@users.noreply.github.com>
2020-06-16 13:58:42 +0700

    Add files via upload

--- bit.c
+++ bit.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(){
-int number=0x00ccbbaa;
+unsigned int number=0x00ccbbaa;
 //printf("%d",number);
 
-for(int i=1;i<5;i++){
-	int shift=(5-i)*8;
+for(int i=0;i<4;i++){
+	int shift=i*8;
 	int totalShift;
 	totalShift=number>>shift;
 	int g=0x000000ff;
-	int total=totalShift&g;
+	int total=totalShift & g;
 	printf("%d element by bit %d \n",i,total);
 	}
 }
