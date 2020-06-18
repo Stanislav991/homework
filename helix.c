831b532528ea974cc70287a0928d02382eab0267
Stanislav991 <66986825+Stanislav991@users.noreply.github.com>
2020-06-18 18:47:06 +0700

    Add files via upload

--- helix.c
+++ helix.c
@@ -1,51 +1,41 @@
 #include <stdio.h>
-#define N 5
+#define N 3
 
 int main(){
 	int helix[N][N];
-	int k=0;
+	int i=0;
+	int j=0;
+	int k=1;
 	int n=N;
 	int row=0;
-	while(n>=row){
-		if(k<4*(n-1)){
-			for(int i=row;i<n;i++){
-				for(int j=row;j<n;j++){
-				while(i==row){
-					for(int f=row;f<n;f++){
-						k++;
-						helix[i][f]=k;
-						}
+while(k<=N*N){
+			i=row;
+				for( j=row;j<n;j++){
+					helix[i][j]=k;
+					k++;
 					}
-				while(j==n-1){
-						for(int g=row;g<n;g++){
-						k++;
-						helix[g][j]=k;
-						}
+			 j=n-1;
+				for(i=row+1;i<n;i++){
+					helix[i][j]=k;
+					k++;
 					}
-				while(i==n-1){
-						for(int f=n;f>row;f--){
-						k++;
-						helix[i][f]=k;
-						}
+			 i=n-1;
+				for(j=n-2;j>row;j--){
+					helix[i][j]=k;
+					k++;
 					}
-				while(j==row){
-						for(int g=n;g>row;g--){
-						k++;
-						helix[g][j]=k;
-						}
+			 j=row;
+				for(i=n-1;i>row;i--){
+					helix[i][j]=k;
+					k++;
 					}
-				}
-			}
-		}
-		else
-		{
-		k++;
 		row++;
 		n--;
-		}
 }
+	//if(N%2)
+	//helix[(N/2)+1][(N/2)+1]=N*N;	
 
-	for(int i=0;i<N;i++){
+for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
 			printf("%d ",helix[i][j]);
 		}
